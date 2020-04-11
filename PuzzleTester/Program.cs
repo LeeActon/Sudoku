3b655760cb46741338585b14495b7669ca901534
Lee Acton <leeacton@0x151.com>
2020-04-10 20:37:01 -0700

    Initial commit

---
+++ PuzzleTester/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace PuzzleTester
+    {
+    class Program
+        {
+        static byte [,] puzzle1 =
+            {
+            { 0, 0, 0,   0, 0, 0,   0, 5, 3 },
+            { 0, 0, 2,   7, 0, 9,   0, 4, 0 },
+            { 0, 0, 7,   8, 0, 0,   0, 0, 0 },
+
+            { 0, 3, 0,   0, 0, 0,   0, 0, 6 },
+            { 0, 0, 0,   9, 0, 1,   0, 0, 0 },
+            { 0, 8, 9,   0, 0, 2,   0, 0, 7 },
+
+            { 4, 0, 0,   0, 0, 0,   2, 0, 0 },
+            { 1, 0, 0,   0, 6, 0,   9, 0, 0 },
+            { 8, 0, 0,   0, 4, 0,   0, 0, 0 },
+            };
+        static void Main(string[] args)
+            {
+            Sudoku.Puzzle puzzle = new Sudoku.Puzzle(puzzle1);
+
+            puzzle.Solve();
+
+            if (puzzle.Solutions.Count > 0)
+                {
+                Console.WriteLine((puzzle.Solutions.Count > 1) ? "Solutions:" : "Solution:");
+                foreach (Sudoku.Puzzle solution in puzzle.Solutions)
+                    {
+                    for (int row = 0; row < 9; row++)
+                        {
+                        if ((row % 3) == 0)
+                            Console.WriteLine();
+
+                        Console.WriteLine(String.Format("{0} {1} {2}  {3} {4} {5}  {6} {7} {8}",
+                            solution.GetValue(row, 0),
+                            solution.GetValue(row, 1),
+                            solution.GetValue(row, 2),
+                            solution.GetValue(row, 3),
+                            solution.GetValue(row, 4),
+                            solution.GetValue(row, 5),
+                            solution.GetValue(row, 6),
+                            solution.GetValue(row, 7),
+                            solution.GetValue(row, 8)
+                            ));
+
+                        }
+                    Console.WriteLine("--------------------------------------");
+                    }
+                }
+            else
+                {
+                Console.WriteLine("No solution found!");
+                }
+            }
+        }
+    }
---
+++ Sudoku/Puzzle.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sudoku
+    {
+    public class Puzzle
+        {
+        private const int rows = 9;
+        private const int columns = 9;
+        private const int rowsInBlock = 3;
+        private const int columnsInBlock = 3;
+
+        private byte [,] data;
+        public int EmptyCells { get; set; }
+
+        private List<Puzzle> solutions;
+        public List<Puzzle> Solutions
+            {
+            get
+                {
+                if (this.solutions == null)
+                    this.solutions = new List<Puzzle>();
+
+                return this.solutions;
+                }
+            }
+
+        public Puzzle()
+            {
+            this.data = new byte[rows,columns];
+            this.solutions = null;
+            }
+
+        public Puzzle(byte [,] newData)
+            {
+            this.data = new byte[rows, columns];
+
+            for (int row = 0; row < rows; row++)
+                {
+                for (int column = 0; column < columns; column++)
+                    {
+                    byte value = newData[row, column];
+
+                    this.data[row, column] = value;
+
+                    if (value == 0)
+                        EmptyCells++;
+                    }
+                }
+            }
+
+        public void Clear()
+            {
+            this.EmptyCells = rows * columns;
+            for (int row = 0; row < rows; row++)
+                {
+                for (int column = 0; column < columns; column++)
+                    {
+                    this.data[row, column] = 0;
+                    }
+                }
+            }
+
+        public void SetValue(int row, int column, byte value)
+            {
+            if (IsValueInUse(row, column, value))
+                value = 0;
+
+            byte oldValue = this.data[row, column];
+
+            this.data[row, column] = value;
+
+            if (oldValue != value)
+                {
+                if (value == 0)
+                    {
+                    EmptyCells++;
+                    }
+                else if (oldValue == 0)
+                    {
+                    EmptyCells--;
+                    }
+                }
+            }
+
+        public byte GetValue(int row, int column)
+            {
+            return this.data[row, column];
+            }
+
+        public bool IsValueInRow(int row, byte value)
+            {
+            for (int column = 0; column < columns; column++)
+                {
+                if (this.data[row, column] == value)
+                    return true;
+                }
+
+            return false;
+            }
+        public bool IsValueInColumn(int column, byte value)
+            {
+            for (int row = 0; row < rows; row++)
+                {
+                if (this.data[row, column] == value)
+                    return true;
+                }
+
+            return false;
+            }
+
+        public bool IsValueInBlock(int testRow, int testColumn, byte value)
+            {
+            int rowBlock = testRow / rowsInBlock;
+            int rowStart = rowBlock * rowsInBlock;
+            int rowEnd = rowStart + rowsInBlock;
+            int columnBlock = testColumn / columnsInBlock;
+            int columnStart = columnBlock * columnsInBlock;
+            int columnEnd = columnStart + columnsInBlock;
+
+            for (int row = rowStart; row < rowEnd; row++)
+                {
+                for (int column = columnStart; column < columnEnd; column++)
+                    {
+                    if (this.data[row, column] == value)
+                        return true;
+                    }
+                }
+
+            return false;
+            }
+
+        public bool IsValueInUse(int row, int column, byte value)
+            {
+            return IsValueInRow(row, value) || IsValueInColumn(column, value) || IsValueInBlock(row, column, value);
+            }
+
+        public void Solve()
+            {
+            for (int row = 0; row < rows; row++)
+                {
+                for (int column = 0; column < columns; column++)
+                    {
+                    if (this.data[row, column] == 0)
+                        {
+                        for (byte value = 1; value <= 9; value++)
+                            {
+                            if (!IsValueInUse(row, column, value))
+                                {
+                                SetValue(row, column, value);
+                                Solve();
+                                SetValue(row, column, 0);
+                                }
+                            }
+
+                        return;
+                        }
+                    }
+                }
+             
+            Solutions.Add(new Puzzle(this.data));
+            }
+        }
+    }
